3608e0b6c65d284f55a74000b38ea9e91efbdd59
Sameer Selokar <sameerselokar924@gmail.com>
2026-07-14 10:33:51 +0530

    Comment out ThemeToggle in navbar
    Comment out the ThemeToggle component in the navbar.

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -43,7 +43,7 @@ export function Navbar() {
         </nav>
 
         <div className="flex items-center gap-2">
-          <ThemeToggle />
+          {/* <ThemeToggle /> */}
           <SignedIn>
             <UserButton afterSignOutUrl="/" />
           </SignedIn>
